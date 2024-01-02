46e23941f9fffcecfb4a21cf181a2f939c79b425
ginacio23 <150558011+ginacio23@users.noreply.github.com>
2024-01-02 16:37:35 -0300

    Create ginacio23.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ginacio23.py
@@ -0,0 +1,16 @@
+# Pagina Web oficial de Python: https://www.python.org
+
+# Sintaxis para crear comentario de una sola linea
+
+"""
+Sintaxis para crear comentarios de mas de una lista
+"""
+
+nueva_variable = "Hola soy una variable"
+
+entero = 99
+float = 9,14
+boolean = True
+cadena = "Hola soy una cadena"
+
+print("¡Hola, Python!")
